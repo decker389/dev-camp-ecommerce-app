24074cffa1b74d84586bd1ef538bb5ceb400c273
christopher decker <decker389@aol.com>
2021-04-02 10:54:28 -0600

    calculating order total and subtotal

--- src/components/order/review.js
+++ src/components/order/review.js
@@ -16,15 +16,27 @@ class Review extends Component {
     }
 
     render() {
+        let subtotal = 0;
+        this.props.cartProducts.map(cartProduct => {
+            subtotal += cartProduct.quantity * cartProduct.product.price;
+        })
+        
         return(
             <div className='review'>
                 <PageTitle className='review__page-title' title='Order Review' />
-                <ReviewForm className='review__form' onSubmit={this.onSubmit}/>
+                <ReviewForm className='review__form' onSubmit={this.onSubmit} subtotal={subtotal}/>
             </div>
         )
     }
 }
 
-Review = connect(null, action)(Review);
+function mapStateToProps(state) {
+    const { cartProducts } = state.user;
+    return {
+        cartProducts
+    }
+}
+
+Review = connect(mapStateToProps, action)(Review);
 
 export default Review;
